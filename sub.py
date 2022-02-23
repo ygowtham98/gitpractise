927a53de8a9137bd7537a9ca1fabc77ec9500061
ygowtham98 <gyemineni7@gmail.com>
2022-02-23 12:28:51 -0500

    sub of two numbers

---
+++ sub.py
@@ -0,0 +1,11 @@
+# Python program to subtract two numbers
+
+# take inputs
+num1 = 10
+num2 = 7
+
+# subtract two numbers
+sub = num1 - num2
+
+# print the subtraction result
+print('The subtraction of numbers =', sub)
